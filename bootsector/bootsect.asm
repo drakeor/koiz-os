d57c182b9fdfa01236ac2aae80fc7e088be73ce7
Drakeor <drakeor.dragon@gmail.com>
2020-12-20 04:39:07 -0800

    Starting work on the boot sector

--- bootsector/bootsect.asm
+++ bootsector/bootsect.asm
@@ -1,5 +1,20 @@
 ; Boot sector
 use16
+
+; Makes printing easier
+macro plet letter* {
+    mov al, letter
+    int 0x10
+}
+
+; Go to TTY mode
+mov ah, 0x0e
+
+plet 'K'
+plet 'o'
+plet 'i'
+plet 'Z'
+
 main_loop:
     jmp main_loop
 
