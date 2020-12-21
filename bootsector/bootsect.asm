4fa6bdc61eb5b2999e3ff21ed369c8abb0e7c136
Drakeor <drakeor.dragon@gmail.com>
2020-12-20 16:49:54 -0800

    Little more work on the boot sector

--- bootsector/bootsect.asm
+++ bootsector/bootsect.asm
@@ -1,22 +1,38 @@
 ; Boot sector
 use16
 
-; Makes printing easier
-macro plet letter* {
-    mov al, letter
-    int 0x10
-}
+; Set global memory offset
+org 0x7c00
 
-; Go to TTY mode
-mov ah, 0x0e
+; Set up the stack
+mov bp, 0x8000
+mov sp, bp
 
-plet 'K'
-plet 'o'
-plet 'i'
-plet 'Z'
+; Print name of BOIS
+mov si, message
+call print_string
 
 main_loop:
     jmp main_loop
+
+; Put the address of the string in si prior to calling. Terminated by 0x00
+print_string:
+    pusha
+    mov ah, 0x0E
+    .p_loop:
+        lodsb
+        cmp al, 0x00
+        je .done
+        int 10h
+        jmp .p_loop
+    .done:
+        popa
+        ret
+
+; Variables
+message:
+    db 'KoiZ OS', 0x00
+
 
 ; Buffer out to 512 bytes
 times 510-($-$$) db 0
