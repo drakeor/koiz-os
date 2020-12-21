876f18ba2f7f5b7c984aa203899605e9e59673b6
Drakeor <drakeor.dragon@gmail.com>
2020-12-21 15:42:19 -0800

    Committing what i have so far

--- kernel/build.sh
+++ kernel/build.sh
@@ -1,5 +1,5 @@
 # Compile for our OS (32 bit)
-cargo build --target i686-koiz_os.json -Z build-std
+# cargo build --target i686-koiz_os.json -Z build-std
 
 # Compile for our OS (64 bit) {NOT YET SUPPORTED}
 # cargo build --target i686-koiz_os.json  -Z build-std
