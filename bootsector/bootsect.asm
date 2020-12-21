ebfe87f7226ada47014e5fc7d29d8cee419a5274
Drakeor <drakeor.dragon@gmail.com>
2020-12-20 18:03:45 -0800

    Made a print hex function

--- bootsector/bootsect.asm
+++ bootsector/bootsect.asm
@@ -12,6 +12,14 @@ mov sp, bp
 mov si, message
 call print_string
 
+; Print out stack location
+mov bx, bp
+mov al, bh
+call print_hex_byte
+mov al, bl
+call print_hex_byte
+
+; Main logic
 main_loop:
     jmp main_loop
 
@@ -29,9 +37,37 @@ print_string:
         popa
         ret
 
+; Assume the byte is already in al
+print_hex_byte:
+    pusha
+    ; Get the higher 4 bits
+    mov dl, al
+    shr al, $4
+    call print_hex
+    ; Get the lower 4 bits
+    mov al, dl
+    mov cl, 0x0F
+    and al, cl
+    call print_hex
+    popa
+    ret
+
+; Assume the byte is already in al
+print_hex:
+    pusha
+    mov ah, 0x0E
+    add al, 0x30
+    cmp al, 0x39
+    jbe .done
+    add al, 0x07
+    .done:
+        int 10h
+        popa
+        ret
+
 ; Variables
 message:
-    db 'KoiZ OS', 0x00
+    db 'KoiZ OS. Stack Start: ', 0x00
 
 
 ; Buffer out to 512 bytes
