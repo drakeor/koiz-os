9bc105183c74e814e4feb1a988c8cd771479b111
Drakeor <drakeor.dragon@gmail.com>
2020-12-22 03:29:18 -0800

    Made it clear this is 32-bit

--- kernel/build.sh
+++ kernel/build.sh
@@ -2,14 +2,14 @@
 mkdir -p ../bin
 mkdir -p ../obj
 
-echo "compiling kernel entry code"
+echo "compiling 32-bit kernel entry code"
 # Build our custom kernel entry executable
 fasm.x64 kernel_entry.asm ../obj/kernel_entry.o
 
-echo "compiling kernel"
+echo "compiling 32-bit kernel"
 # Compile the kernel
 gcc -m32 -ffreestanding -c main.c -o ../obj/main.o -fno-pie
 
-echo "linking kernel"
+echo "linking 32-bit kernel"
 # Link everything together
 ld -o ../bin/kernel.bin -Ttext 0x1000 ../obj/kernel_entry.o ../obj/main.o --oformat binary -m elf_i386
