862ac0b4f58dd6b2842c5c259cb494f1bb265ba9
Drakeor <drakeor.dragon@gmail.com>
2020-12-31 00:38:15 -0800

    More code cleanup

--- kernel/core/interrupt_handler.c
+++ kernel/core/interrupt_handler.c
@@ -1,10 +1,6 @@
 #include "interrupt_handler.h"
 #include "../libc/stdlib.h"
 
-/*void handle_interrupt(void)
-{
-    print("Handling an interrupt!\n");
-}*/
 
 /* Represents an entry in the descriptor table */
 struct interrupt_descriptor_t {
@@ -19,17 +15,6 @@ extern struct interrupt_descriptor_t idt_start;
 extern uint32_t idt_info;
 
 extern uintptr_t isr_0;
-
-void interrupt_handler(struct cpu_state cpu, struct stack_state stack, uint32_t interrupt)
-{
-    
-    /*int i;
-    for(i = 0; i < 10; i++) {
-        printf("Handling an interrupt %d: %x!\n", i,  *((&idt_start)+i));
-    }*/
-    printf("Handling interrupt %x", interrupt);
-}
-
 
 /*
  * This function populates the interrupt descriptor table
@@ -48,17 +33,4 @@ void setup_idt(void)
 
     printf("Interrupt 0x0: Offset High/Low: %x %x | Selector: %x | Flags: %x \n", idt_start.offset_high, 
         idt_start.offset_low, idt_start.selector, idt_start.flags);
-
-    /* Might have been easier to load it into as a 64-bit and
-     * manipulate it but whatever */
-    /*idt_size = (uint16_t)idt_info[0] << 8 | idt_info[1];
-    idt_start_addr = idt_info[2] << 24 
-        | idt_info[3] << 16
-        | idt_info[4] << 8
-        | idt_info[5];*/
-    //idt_size = 
-
-    //printf("IDT Size: %x , Start Address: %x\n", idt_size, idt_start_addr);
-    //idt_info = 0x100;
-    //printf("IDT Size: %d | %x\n", idt_info, idt_info);
 }
--- kernel/core/interrupt_handler.h
+++ kernel/core/interrupt_handler.h
@@ -3,42 +3,6 @@
 
 #include <stdint.h>
 
-//void handle_interrupt(void);
-
 void setup_idt(void);
-
-//extern uint8_t idt_info[6];
-//extern uint64_t idt_info[256];
-
-/* 
- * Source: Little OS Book
- * At the time of the interrupts,
- * the stack will look like this:
- * [esp+12] eflags
- * [esp+8] cs
- * [esp+4] eip
- * [esp] error_code
- */
-
-struct cpu_state {
-    uint32_t eax;
-    uint32_t ebx;
-    uint32_t ecx;
-    uint32_t edx;
-    uint32_t esi;
-    uint32_t edi;
-    uint32_t esp;
-} __attribute__((packed));
-
-
-struct stack_state {
-    uint32_t error_code;
-    uint32_t eip;
-    uint32_t cs;
-    uint32_t eflags;
-} __attribute__((packed));
-
-
-void interrupt_handler(struct cpu_state cpu, struct stack_state stack, uint32_t interrupt);
 
 #endif
