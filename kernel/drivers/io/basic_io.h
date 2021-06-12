a357643da211602c100a8dde7ff8a6fd1188c5e7
River B <clearflare@live.com>
2021-06-11 23:04:16 -0700

    Refactored the IO stuff

--- kernel/drivers/io/basic_io.h
+++ kernel/drivers/io/basic_io.h
@@ -3,20 +3,43 @@
 
 #include <stdint.h>
 
-/*
- * Writes data to an IO port
+/**
+ * io_byte_in() - Reads a byte of data from an IO port
+ * 
+ * @port:   Port to read from
+ * 
+ * Returns byte of data read from IO port
  */
 uint8_t io_byte_in(uint16_t port);
+
+/**
+ * io_word_in() - Reads a word of data from an IO port
+ * 
+ * @port:   Port to read from
+ * 
+ * Returns a word of data read from IO port
+ */
 uint16_t io_word_in(uint16_t port);
 
-/*
- * Reads data from an IO port
+/**
+ * io_byte_out() - Writes a byte of data to the IO port
+ * 
+ * @port:   Port to write to
+ * @data:   Byte of data to write
  */
 void io_byte_out(uint16_t port, uint8_t data);
+
+/**
+ * io_word_out() - Writes a word of data to the IO port
+ * 
+ * @port:   Port to write to
+ * @data:   Word of data to write
+ */
 void io_word_out(uint16_t port, uint16_t data);
 
-/* Waits an an I/O operation to complete */
+/**
+ * io_wait() - Waits for some time
+ */
 void io_wait(void);
-
 
 #endif
